f9a06c2b5587dc577d13ed782698c72a159e0f5c
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-10 15:12:19 +0530

    Create banana.c

---
+++ banana.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	float d;
+	printf("enter the total [prize of apple and banana");
+	scanf("%d",&a);
+	printf("the total prize of apple is");
+	scanf("%d",&b);
+	c=a-b;
+	printf("the one dazon banana prize is %d",c);
+	d=c/12;
+	printf("the prize of one banana is %f",d);
+	
+	
+}
