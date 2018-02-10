f021a45c934dadda38cb99c71c1949f41e259f4a
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-10 15:05:48 +0530

    Create profityarn.c

---
+++ profityarn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("\n enter the selling prize");
+	scanf("%d",&a);
+	printf("\n enter the cost of things");
+	scanf("%d",&b);
+	c=a-b;
+	printf("\n the profit value is %d",c);
+}
+
+	
+Click here to Reply 
